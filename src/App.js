77058e3f3efcdfe073f1d1b34ec434db3586b820
Anup@453 <aghattikar82@gmail.com>
2025-02-24 20:24:06 +0530

    Added SpeedIngights

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Form } from './Component/Form';
 
 
 function App() {
   return (
     <>
-
+    <SpeedInsights />
     <Form/>
     </>
   );
